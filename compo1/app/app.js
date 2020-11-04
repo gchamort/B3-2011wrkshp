742045c33c810319eca5f81e08e8587fd2b2dfae
gchamort <guillaume.p.chanut@gmail.com>
2020-11-04 10:49:46 +0100

    ouais

--- compo1/app/app.js
+++ compo1/app/app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end("Groupe 8 présidents !");
 });
 
 server.listen(port, hostname, () => {
